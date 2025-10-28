bed6eeb52075f89e776c39e9b80d175e984586ae
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2025-10-28 21:35:38 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// 1.1.1 npm init -y
+// 1.1.2 npm install --save-dev nodemon
+// 1.1.3 "scripts": {
+//      "dev": "nodemon src/index.js"
+//       }
+// 1.1.4 npm init @eslint/config@latest
+
+
+
+
+const message = "Hello";
+console.log(message);
