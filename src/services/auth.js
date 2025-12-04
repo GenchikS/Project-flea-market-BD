5a143ed18d046908a42fe48b0b99190b4f213c99
gpazi <g.paziy@gmail.com>
2025-12-05 00:03:38 +0200

    dataUser

--- src/services/auth.js
+++ src/services/auth.js
@@ -42,7 +42,7 @@ export const postLoginUser = async (payload) => {
   const accessToken = randomBytes(30).toString(`base64`);
   const refreshToken = randomBytes(30).toString(`base64`);
   return await SessionsCollection.create({
-    // data: user,
+    user,
     idUser: user._id,
     accessToken,
     refreshToken,
